262b505673a60fa14738bfb4752672ebdee17f2d
icegit40 <mayeravi2001@gmail.com>
2026-02-24 21:44:40 +0000

    updated backend

--- prod/backend.tf
+++ prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = ""ice-backend-bucket""
+    bucket         = "ice-backend-bucket"
     region         = "us-west-2"
     dynamodb_table = "terraform-state-locked"
   }
