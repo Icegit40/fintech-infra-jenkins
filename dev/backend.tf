aeb1d3fecb78881dcafb869cdbed08a948c960a5
icegit40 <mayeravi2001@gmail.com>
2026-02-24 15:43:51 +0000

    update

--- dev/backend.tf
+++ dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "jenkins.pgabootcam.org"
-    bucket         = "projectgroup-a-terraform-backend-bucket"
+    bucket         = projectgroup-b-terraform-backend-bucket"
     region         = "us-west-2"
     dynamodb_table = "terraform-state-locking"
   }
